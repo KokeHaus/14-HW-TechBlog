6950dba2a7d6ce80a647387e5c7d53fc7e1b9ba6
KokeHaus <austinbcw@gmail.com>
2022-05-11 04:34:17 -0500

    models

--- Develop/models/Comment.js
+++ Develop/models/Comment.js
@@ -3,16 +3,39 @@ const sequelize = require('../config/config');
 
 class Comment extends Model {}
 
-Comment.init(
-  {
-    body: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
+Comment.init({
+  id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
   },
-  {
-    sequelize
-  }
-);
+  comment_text: {
+      type: DataTypes.STRING,
+      validate: {
 
+          len: [3]
+      }
+  },
+  user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+          model: 'user',
+          key: 'id'
+      }
+  },
+  post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+          model: 'post',
+          key: 'id'
+      }
+  }
+},{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  });
 module.exports = Comment;
